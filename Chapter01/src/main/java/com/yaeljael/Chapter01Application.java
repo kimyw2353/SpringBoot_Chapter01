9bf581eedcdf9d82d23f637d8b6557d7086c46dc
kimyw2353 <kimyw2353@naver.com>
2022-07-06 04:30:11 +0900

    applicationType 변경

--- Chapter01/src/main/java/com/yaeljael/Chapter01Application.java
+++ Chapter01/src/main/java/com/yaeljael/Chapter01Application.java
@@ -1,13 +1,16 @@
 package com.yaeljael;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Chapter01Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Chapter01Application.class, args);
+		SpringApplication application = new SpringApplication(Chapter01Application.class);
+		application.setWebApplicationType(WebApplicationType.NONE);
+		application.run(args);
 	}
 
 }
