7a4e61fe8c012004fa46ee2ffe52c2f5dd6f8772
kimyw2353 <kimyw2353@naver.com>
2022-07-15 01:54:53 +0900

    console banner 변경

--- Chapter01/src/main/java/com/yaeljael/Chapter01Application.java
+++ Chapter01/src/main/java/com/yaeljael/Chapter01Application.java
@@ -1,5 +1,6 @@
 package com.yaeljael;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +11,10 @@ public class Chapter01Application {
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(Chapter01Application.class);
 		application.setWebApplicationType(WebApplicationType.NONE);
+		
+		application.setBannerMode(Banner.Mode.OFF);
 		application.run(args);
+		
 	}
 
 }
